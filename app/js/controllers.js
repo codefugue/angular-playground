58f95a9f7a21377a847e213d525f8882716ed990
indraneelr <indraneelrr@gmail.com>
2013-11-20 01:17:41 +0530

    tree display

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,33 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+angular.module('myApp.controllers', ['myApp.services']).
+  controller('MyCtrl1',['$scope','myService',function($scope,myService) {
+
+  	var items = [
+  		{ name:"item1",value:"23"},
+  		{ name:"item2",value:"45"},
+  		{ name:"item3",value:"67"},
+  		{ name:"item4",value:"34"}
+  	];
+  	$scope.loadData = function(){
+  		$scope.list = items;
+  	};
+
+  	
 
   }])
-  .controller('MyCtrl2', [function() {
+  .controller('MyCtrl2', ['$scope','myService',function($scope,myService){
+  
+  	$scope.file = {};
+  	$scope.getFile = function(filename){
+  		$scope.file[filename] = $scope.file[filename] || myService.getFileContent(filename);
+  		$scope.currentFile = $scope.file[filename];
+  	}
 
+  	$scope.folders = myService.getTree();
+
+  	$scope.addNewPerson = function(){
+  		$scope.currentFile.push({name:"",age:""});
+  	};
   }]);
--- app/js/services.js
+++ app/js/services.js
@@ -5,5 +5,57 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('myApp.services', []).
-  value('version', '0.1');
+angular.module('myApp.services', [])
+ .service("myService",function(){
+ 	var fileContents = {
+ 		"f1_file1": [
+ 			{name: "Moroni", age: 50},
+            {name: "Tiancum", age: 43},
+         ],
+         "f1_file2": [
+         	{name: "Jacob", age: 27},
+         	{name: "Nephi", age: 29},
+         	{name: "Enos", age: 34}
+         ],
+		 "f1_file3": [
+		 	{name: "Enos", age: 34},
+         	{name: "Jacob", age: 27},
+         	{name: "Tiancum", age: 43}
+         ],
+
+		 "f2_file1": [
+		 	{name: "werer", age: 34},
+         	{name: "herta", age: 54},
+         	{name: "Tumerga", age: 56}
+         ],
+
+ 	}
+
+ 	var getTree = function(){
+ 		return [
+ 			{	
+				name: "folder1",
+				files : [
+					{filename : "f1_file1"},
+					{filename : "f1_file2"},
+					{filename : "f1_file3"}
+				]
+ 			},
+ 			{
+				name: "folder2",
+				files : [
+					{filename : "f2_file1"},
+				]
+ 			} 			
+ 		]
+ 	};
+
+ 	var getFileContent = function(filename){
+ 		return fileContents[filename];
+ 	};
+
+ 	return {
+ 		"getTree" : getTree,
+ 		"getFileContent" : getFileContent
+ 	}
+ })
